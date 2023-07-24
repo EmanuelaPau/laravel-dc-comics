97f59f8cbc3fe744dbfb3326d8e9b16f9b3e28c6
EmanuelaPau <uapame@gmail.com>
2023-07-24 15:22:45 +0200

    Add migration

---
+++ database/migrations/2023_07_24_131348_create_comics_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title', 60);
+            $table->string('description', 500);
+            $table->string('price', 9);
+            $table->string('series', 20);
+            $table->string('type', 20);
+            $table->date('sale_date');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
