079a0923bd5816896a0d96ff7a2df9809a6b0719
EmanuelaPau <uapame@gmail.com>
2023-07-26 15:10:29 +0200

    Add value to form

--- resources/views/admin/comics/edit.blade.php
+++ resources/views/admin/comics/edit.blade.php
@@ -19,27 +19,27 @@
                     @method("PUT")
                     <div class="mb-3">
                         <label for="title" class="form-label">Write here the Comic Title</label>
-                        <input type="text" class="form-control" id="title" aria-describedby="emailHelp" name="title">
+                        <input type="text" class="form-control" id="title" aria-describedby="emailHelp" name="title" value="{{$comic->title}}">
                     </div>
                     <div class="mb-3">
                         <label for="description" class="form-label">Write here the Comic description</label>
-                        <input type="text" class="form-control" id="description" aria-describedby="emailHelp" name="description">
+                        <input type="text" class="form-control" id="description" aria-describedby="emailHelp" name="description " value="{{$comic->description}}">
                     </div>
                     <div class="mb-3">
                         <label for="price" class="form-label">Write here the Comic price</label>
-                        <input type="text" class="form-control" id="price" aria-describedby="emailHelp" name="price">
+                        <input type="text" class="form-control" id="price" aria-describedby="emailHelp" name="price"  value="{{$comic->price}}">
                     </div>
                     <div class="mb-3">
                         <label for="series" class="form-label">Write here the Comic series</label>
-                        <input type="text" class="form-control" id="series" aria-describedby="emailHelp" name="series">
+                        <input type="text" class="form-control" id="series" aria-describedby="emailHelp" name="series" value="{{$comic->series}}">
                     </div>
                     <div class="mb-3">
                         <label for="type" class="form-label">Write here the Comic type</label>
-                        <input type="text" class="form-control" id="type" aria-describedby="emailHelp" name="type">
+                        <input type="text" class="form-control" id="type" aria-describedby="emailHelp" name="type" value="{{$comic->type}}">
                     </div>
                     <div class="mb-3">
                         <label for="sale_date" class="form-label">Write here the sale date</label>
-                        <input type="text" class="form-control" id="sale_date" aria-describedby="emailHelp" name="sale_date">
+                        <input type="text" class="form-control" id="sale_date" aria-describedby="emailHelp" name="sale_date" value="{{$comic->sale_date}}">
                     </div>
 
                     <button type="submit" class="btn btn-primary">Submit</button>
