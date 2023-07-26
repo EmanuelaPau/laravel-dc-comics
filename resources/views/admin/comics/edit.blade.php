62698f290a450815c7c0e8e15b1f08f2d8376829
EmanuelaPau <uapame@gmail.com>
2023-07-26 15:01:04 +0200

    Add edit page

--- resources/views/admin/comics/edit.blade.php
+++ resources/views/admin/comics/edit.blade.php
@@ -1,20 +1,17 @@
 @extends('layouts.app')
 
-@section('title','Admin Comics Index')
+@section('title','Admin Comics Show')
 
 @section('main-section')
 
-    {{-- Jumbo  --}}
-    {{-- <div class="my_jumbo d-flex align-items-center ps-5">
-        <div class="j-txt-box">
-            <h5 class="text-uppercase">
-                Welcome to Comics 
-            </h5>
-        </div>
-    </div> --}}
 
     <div class="container">
         <div class="row">
+            <div class="col-12">
+                <h1>
+                    Comic Title: {{$comic->title}}
+                </h1>
+            </div>
             <div class="col-12">
                 <table class="table table-hover">
                     <thead>
@@ -25,12 +22,10 @@
                           <th scope="col">Series</th>
                           <th scope="col">Type</th>
                           <th scope="col">Sale Date</th>
-                          <th scope="col">View</th>
                         </tr>
                     </thead>
 
                       <tbody>
-                        @foreach ( $comicList as $comic)
                         <tr>
                             <th scope="row">{{$comic->title}}</th>
                             <td>{{$comic->description}}</td>
@@ -38,11 +33,7 @@
                             <td>{{$comic->series}}</td>
                             <td>{{$comic->type}}</td>
                             <td>{{$comic->sale_date}}</td>
-                            <td><a class="btn btn-primary mb-2" href="{{ route('admin.comics.show', $comic->id)}}" role="button">View</a>
-                                <a class="btn btn-warning mb-2" href="{{ route('admin.comics.show', $comic->id)}}" role="button">Edit</a>
-                                <a class="btn btn-danger mb-2" href="{{ route('admin.comics.show', $comic->id)}}" role="button">Delete</a></td>
                         </tr>
-                        @endforeach
                       </tbody>
                 </table>
             </div>
