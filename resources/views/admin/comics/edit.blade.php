9c0e4d7a344f015e2dda32aae5407f18d342b8db
EmanuelaPau <uapame@gmail.com>
2023-07-26 15:06:15 +0200

    Add content to edit.blade.php

--- resources/views/admin/comics/edit.blade.php
+++ resources/views/admin/comics/edit.blade.php
@@ -1,43 +1,53 @@
 @extends('layouts.app')
 
-@section('title','Admin Comics Show')
+@section('title','Admin Comics Create')
 
 @section('main-section')
-
-
-    <div class="container">
+    <div class="container mb-5">
         <div class="row">
             <div class="col-12">
-                <h1>
-                    Comic Title: {{$comic->title}}
+                <h1 class="text-center">
+                    Create new Comic
                 </h1>
-            </div>
-            <div class="col-12">
-                <table class="table table-hover">
-                    <thead>
-                        <tr>
-                          <th scope="col">Title</th>
-                          <th scope="col">Description</th>
-                          <th scope="col">Price</th>
-                          <th scope="col">Series</th>
-                          <th scope="col">Type</th>
-                          <th scope="col">Sale Date</th>
-                        </tr>
-                    </thead>
-
-                      <tbody>
-                        <tr>
-                            <th scope="row">{{$comic->title}}</th>
-                            <td>{{$comic->description}}</td>
-                            <td>{{$comic->price}}</td>
-                            <td>{{$comic->series}}</td>
-                            <td>{{$comic->type}}</td>
-                            <td>{{$comic->sale_date}}</td>
-                        </tr>
-                      </tbody>
-                </table>
             </div>
         </div>
 
+        <div class="row mb-3">
+            <div class="col-9 m-auto">
+                <form action="{{route('admin.comics.store')}}" method="POST">
+                    @csrf
+                    @method("PUT")
+                    <div class="mb-3">
+                        <label for="title" class="form-label">Write here the Comic Title</label>
+                        <input type="text" class="form-control" id="title" aria-describedby="emailHelp" name="title">
+                    </div>
+                    <div class="mb-3">
+                        <label for="description" class="form-label">Write here the Comic description</label>
+                        <input type="text" class="form-control" id="description" aria-describedby="emailHelp" name="description">
+                    </div>
+                    <div class="mb-3">
+                        <label for="price" class="form-label">Write here the Comic price</label>
+                        <input type="text" class="form-control" id="price" aria-describedby="emailHelp" name="price">
+                    </div>
+                    <div class="mb-3">
+                        <label for="series" class="form-label">Write here the Comic series</label>
+                        <input type="text" class="form-control" id="series" aria-describedby="emailHelp" name="series">
+                    </div>
+                    <div class="mb-3">
+                        <label for="type" class="form-label">Write here the Comic type</label>
+                        <input type="text" class="form-control" id="type" aria-describedby="emailHelp" name="type">
+                    </div>
+                    <div class="mb-3">
+                        <label for="sale_date" class="form-label">Write here the sale date</label>
+                        <input type="text" class="form-control" id="sale_date" aria-describedby="emailHelp" name="sale_date">
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Submit</button>
+
+                    <button type="reset" class="btn btn-warning">Reset</button>
+                </form>
+            </div>
+        </div>
     </div>
+    
 @endsection
